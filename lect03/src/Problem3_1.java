0f3305f125f69a960d855699ea08f8c2cfaa04b9
patchara-akkaraprasit <77107573+patchara-akkaraprasit@users.noreply.github.com>
2021-01-21 14:42:20 +0700

    Update Problem3_1.java

--- lect03/src/Problem3_1.java
+++ lect03/src/Problem3_1.java
@@ -3,12 +3,37 @@
 // Last Name:
 //
 // Team Members:
-// 1. <student id> <first name> <last name>
-// 2.
-// 3.
+// 1.6030300741 พัชร อัครประสิทธิ์
+// 2.6030300318 ณัฐพงค์ ตื่มสูงเนิน
+// 3.6030300075 กัลยารัตน์ ศรีชัย
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Problem3_1 {
     public static void main(String[] args) {
         // TODO
+        Scanner sn = new Scanner(System.in);
+        int number;
+        int number2;
+        int i = 1;
+        int sum=0;
+        System.out.println("Input");
+        number = sn.nextInt();
+
+        while ( i <= number ) {
+            System.out.print(">");
+            number2 = sn.nextInt();
+            ++i;
+            if(number2%4==0)
+            {
+                if(sum<number2)
+                {
+                    sum=number2;
+                }
+            }
+
+        }
+        System.out.println("Output");     
+        System.out.print(sum);
     }
 }
